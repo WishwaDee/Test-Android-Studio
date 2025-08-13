45cc01eddae3b39c80c977bb6c5bc74493a74b2a
Wishwa Dilshan <wishwadilshan628@gmail.com>
2025-08-13 14:53:52 +0530

    fixed issues

--- test.kt
+++ test.kt
@@ -1,11 +1,9 @@
-class test {
-    fun main() {
-        println("Hello, World!")
+fun main() {
+    println("Hello, World!")
 
-        val num1 = 5
-        val num2 = 3
-        val sum = num1 + num2
+    val num1 = 5
+    val num2 = 3
+    val sum = num1 + num2
 
-        println("The sum of $num1 and $num2 is $sum")
-    }
+    println("The sum of $num1 and $num2 is $sum")
 }
