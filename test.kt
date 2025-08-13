0ddb0bf47a14c5195a801a76e367eea49e08129e
Wishwa Dilshan <wishwadilshan628@gmail.com>
2025-08-13 14:50:12 +0530

    First Commit

---
+++ test.kt
@@ -0,0 +1,11 @@
+class test {
+    fun main() {
+        println("Hello, World!")
+
+        val num1 = 5
+        val num2 = 3
+        val sum = num1 + num2
+
+        println("The sum of $num1 and $num2 is $sum")
+    }
+}
